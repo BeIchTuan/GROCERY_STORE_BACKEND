104a62278d1438686e820187703f114042feac4d
BeIchTuan <22521595@gm.uit.edu.vn>
2024-11-19 22:54:17 +0700

    initial project

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+// const { MongoClient } = require('mongodb');
+const routes = require('./routes');
+const app = express();
+const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+//const cookieParser = require('cookie-parser');
+const cors = require("cors");
+
+dotenv.config();
+app.use(morgan('combined'));
+
+// app.use(cookieParser());
+
+//const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
+// Sử dụng middleware cors
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Cho phép gửi cookie
+  })
+);
+
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+routes(app);
+
+// Lắng nghe server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log('Mongoose connected to MongoDB');
+//     // Now, you can safely start your server and perform database operations
+//     server.listen(port, () => {
+//       console.log(`App listening on port ${port}`);
+//     });
+//   })
+//   .catch(err => {
+//     console.error('Mongoose connection error:', err);
+//   });
+
