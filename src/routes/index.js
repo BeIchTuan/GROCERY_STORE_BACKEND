11a704ed06d248d4568418bf0a86658f708c7f35
BeIchTuan <22521595@gm.uit.edu.vn>
2024-12-19 22:47:27 +0700

    create API for customer

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const UserRouter = require("./UserRouter");
 const CategoryRouter = require("./CategoryRouter");
 const ProductRouter = require("./ProductRouter");
 const InvoiceRouter = require("./InvoiceRouter");
+const CustomerRouter = require("./CustomerRouter");
 
 const routes = (app) => {
   app.use("/", UserRouter);
   app.use("/", CategoryRouter);
   app.use("/", ProductRouter);
   app.use("/", InvoiceRouter);
+  app.use("/", CustomerRouter);
 };
 
 module.exports = routes;
