a7158c2cd02cfc1ff8a18633e2938fc18cd79546
Tú <22521591@gm.uit.edu.vn>
2024-12-20 16:41:08 +0700

    update allowedOrigins for CORS configuration

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(morgan('combined'));
 
 app.use(cookieParser());
 
-//const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5177"];
 
 // Sử dụng middleware cors
 app.use(
